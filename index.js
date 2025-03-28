5c5045c03984c9db9fb9b144d1f30abe45753329
Rithik-02 <rithirithi246863@gmail.com>
2025-03-28 21:51:22 +0530

    refactor: Update user_id retrieval to use localStorage for dynamic merchant ID access

--- index.js
+++ index.js
@@ -147,8 +147,8 @@ const audio = new Audio(
   const isMobile = window.matchMedia("(max-width: 767px)").matches;
   let CONFIG = [];
   let INTERACTION_DATA = [];
-  // const user_id = localStorage.getItem("merchantId");
-  const user_id = "82408252-28a4-422d-94be-e1c5fba157d0";
+  const user_id = localStorage.getItem("merchantId");
+  // const user_id = "82408252-28a4-422d-94be-e1c5fba157d0";
 
   // ============================================= MODEL INITIALIZATION AND CONFIGURATION FUNCTIONS =============================================
 
