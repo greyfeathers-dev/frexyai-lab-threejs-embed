fa04551d344ef933c3a98f54c4035d664fc3e55a
Rithik-02 <rithirithi246863@gmail.com>
2025-06-02 20:04:32 +0530

    Reintroduce alert for checkPageVisits in audio upload process

--- index.js
+++ index.js
@@ -3334,7 +3334,6 @@ async function uploadAudioToStorage(audioBlob, interactionName) {
       // 2. The current page hasn't been scrolled past 70%
       if (!wasConfusedInteractionPageScrolled(currentPath)) {
         if (!hasConfusedInteractionPageBeenVisited(currentPath)) {
-          alert("incrementing");
           incrementConfusedInteractionVisits(currentPath);
           this.checkPageVisits();
         }
@@ -3342,6 +3341,7 @@ async function uploadAudioToStorage(audioBlob, interactionName) {
     }
 
     checkPageVisits() {
+      alert("checkPageVisits");
       if (hasConfusedInteractionTriggered()) return;
       if (hasConfusedInteractionAnyPageScrolled()) return; // Don't trigger if any page was scrolled
 
