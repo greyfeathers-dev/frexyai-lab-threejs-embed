d5d4d803b290a97c1e2c3e0507fb83a2f9b536ca
Rithik-02 <rithirithi246863@gmail.com>
2025-06-04 12:27:47 +0530

    Refine canvas and input container positioning for better mobile layout

--- index.js
+++ index.js
@@ -455,9 +455,9 @@ async function uploadAudioToStorage(audioBlob, interactionName) {
     document.body.appendChild(canvas);
     canvas.style.position = "fixed";
     canvas.style.bottom = isMobile ? "-38px" : "-40px";
-    canvas.style.right = isMobile ? "0px" : "-18px";
+    canvas.style.right = isMobile ? "-0px" : "-18px";
     canvas.style.height = isMobile ? "260px" : "400px";
-    canvas.style.width = isMobile ? "80px" : "280px";
+    canvas.style.width = isMobile ? "130px" : "280px";
     canvas.style.zIndex = "10";
     // canvas.style.backgroundColor = "red";
 
@@ -1972,7 +1972,7 @@ async function uploadAudioToStorage(audioBlob, interactionName) {
     // Positioning of the input box
     inputContainer.style.position = "fixed";
     inputContainer.style.bottom = isMobile ? "8px" : "30px";
-    inputContainer.style.right = isMobile ? "65px" : "165px";
+    inputContainer.style.right = isMobile ? "95px" : "165px";
 
     // Add the input element to the body
     document.body.appendChild(inputContainer);
