91873b4b5bee685bf512f63ff1c86642fc7b0b41
Rithik-02 <rithirithi246863@gmail.com>
2025-10-27 16:17:24 +0530

    Update chatbot page URL and script loading source for staging environment

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ const ELEVENLABS_VOICE_ID = "CYw3kZ02Hs0563khs1Fj"; // Replace with your desired
 const ELEVENLABS_API_URL = "https://api.elevenlabs.io/v1/text-to-speech";
 
 // const CHATBOT_PAGE = "https://frexyai-lab-saas-dashboard-development.vercel.app";
-const CHATBOT_PAGE = "http://localhost:3000";
+// const CHATBOT_PAGE = "http://localhost:3000";
+const CHATBOT_PAGE = "frexyai-lab-saas-dashboard-staging-new.vercel.app";
 const ENDPOINT = "https://node-service-1e6u.onrender.com";
 
 // ***************************************************************************************************************************************************
--- load.js
+++ load.js
@@ -58,8 +58,8 @@ loadScript("https://cdn.jsdelivr.net/npm/three@0.139.0/build/three.min.js")
   )
   .then(() =>
     loadScript(
-      "https://frexyai-lab-threejs-embed-development.vercel.app/index.js"
-    )
+      "https://frexyai-lab-threejs-embed-pre-staging.vercel.app/index.js"
+    ) 
   )
   .then(() => console.log("All scripts loaded successfully!"))
   .catch((error) => console.error("Error loading scripts:", error));
