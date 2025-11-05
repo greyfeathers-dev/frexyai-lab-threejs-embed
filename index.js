9594793a61c2109ae819e4815abd04d740d6bea8
Rooban <roobandc8@gmail.com>
2025-11-05 09:40:38 +0530

    Add UUID validation function to enhance lead ID management; ensure valid UUIDs are used for lead ID assignment, improving data integrity and troubleshooting capabilities.

--- index.js
+++ index.js
@@ -1297,6 +1297,11 @@ async function uploadAudioToStorage(audioBlob, interactionName) {
       });
    }
 
+   const isValidUUID = (str) => {
+      const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+      return uuidRegex.test(str);
+   };
+
    function setLeadId() {
       alert("setLeadId");
       const id = localStorage.getItem("leadId");
